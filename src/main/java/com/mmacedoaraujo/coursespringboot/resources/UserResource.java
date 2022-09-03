58e82f8ea51fc729c5738918e339b1f214bf9700
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-09-03 02:51:24 -0300

    updated findAll method

--- src/main/java/com/mmacedoaraujo/coursespringboot/resources/UserResource.java
+++ src/main/java/com/mmacedoaraujo/coursespringboot/resources/UserResource.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,13 +16,13 @@ import com.mmacedoaraujo.coursespringboot.domain.User;
 public class UserResource {
 
 	@GetMapping
-	public List<User> findAll() {
+	public ResponseEntity<List<User>> findAll() {
 		User maria = new User("1", "Maria Brown", "maria@gmail.com");
 		User alex = new User("2", "Alex Green", "alex@gmail.com");
 		List<User> list = new ArrayList<>();
 		list.addAll(Arrays.asList(maria, alex));
 
-		return list;
+		return ResponseEntity.ok().body(list);
 	}
 
 }
