31bac403cccf2748602684dca6d50fb4ae23d73e
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-09-04 22:27:36 -0300

    package dto and UserDTO

--- src/main/java/com/mmacedoaraujo/coursespringboot/config/Instatiantion.java
+++ src/main/java/com/mmacedoaraujo/coursespringboot/config/Instatiantion.java
@@ -4,17 +4,21 @@ import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
 
 import com.mmacedoaraujo.coursespringboot.domain.User;
 import com.mmacedoaraujo.coursespringboot.repository.UserRepository;
 
-public class Instatiation implements CommandLineRunner {
+@Configuration
+public class Instatiantion implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		userRepository.deleteAll();
 
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
