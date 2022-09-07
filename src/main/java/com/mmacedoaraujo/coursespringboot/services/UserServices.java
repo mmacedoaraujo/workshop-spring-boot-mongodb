341dcfb38d9c06f16f89a26906e4f32a99f8b304
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-09-07 14:11:58 -0300

    findById

--- src/main/java/com/mmacedoaraujo/coursespringboot/services/UserServices.java
+++ src/main/java/com/mmacedoaraujo/coursespringboot/services/UserServices.java
@@ -1,12 +1,14 @@
 package com.mmacedoaraujo.coursespringboot.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mmacedoaraujo.coursespringboot.domain.User;
 import com.mmacedoaraujo.coursespringboot.repository.UserRepository;
+import com.mmacedoaraujo.coursespringboot.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserServices {
@@ -17,8 +19,19 @@ public class UserServices {
 	public List<User> findAll() {
 		return userRepo.findAll();
 	}
+	
+	public User findById(Long id) {
+		Optional<User> user =  userRepo.findById(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		
+		return user.get();
+	}
 
 	public User insert(User user) {
 		return userRepo.save(user);
 	}
+	
+	
 }
